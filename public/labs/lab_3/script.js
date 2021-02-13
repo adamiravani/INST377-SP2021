81cacc3688c72f9af15def754e2b73c18a77c389
adamiravani <78195922+adamiravani@users.noreply.github.com>
2021-02-12 21:16:50 -0500

    final

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,24 @@
-/* Put your javascript in here */
+/* configuration */
+let width = 130; // image width
+let count = 3; // visible images count
+
+let list = container.querySelector("ul");
+let listElems = container.querySelectorAll("li");
+
+let position = 0; // ribbon scroll position
+
+container.querySelector(".prev").onclick = function () {
+  // shift left
+  position += width * count;
+  // can't move to the left too much, end of images
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + "px";
+};
+
+container.querySelector(".next").onclick = function () {
+  // shift right
+  position -= width * count;
+  // can only shift the ribbbon for (total ribbon length - visible count) images
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + "px";
+};
