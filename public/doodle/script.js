76c5a1b651dbcce210400fa05f77e3339ad95f40
adamiravani <78195922+adamiravani@users.noreply.github.com>
2021-02-19 04:30:07 -0500

    what is happenning

--- public/doodle/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded') => {
-    const grid = document.querySelector('.grid')
-    const doodler = document.createElement('div')
-
-    function createDoodler() {
-        grid.appendChild(doodler)
-    }
-    createDoodler()
-}
